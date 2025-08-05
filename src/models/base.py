f06129bd35d73eb55e52d89f0561e7df639731b8
A3Ron <ronny.saba@proton.me>
2025-08-05 15:40:11 +0200

    savepoint

--- src/models/base.py
+++ src/models/base.py
@@ -1,5 +1,8 @@
 from sqlalchemy import Column, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime, timezone
+
+Base = declarative_base()
 
 class TimestampMixin:
     created_at = Column(DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
